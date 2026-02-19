08146a97abe6ebc38f3257ed47174b80f0074cac
Bunya <bunyamin6331@gmail.com>
2026-02-19 18:21:40 +0300

    Js dosyası dışarıya alındı

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+         const isim ="mehmet";
+            var soyisim ="yilmaz";
+            var tamisim = isim+ " "+ soyisim;
+            var yetiskinmi = yas !== "18";
+            var buYil = new Date().getFullYear();
+            //isim = "mehmet";
+            console.log(!true);
+            console.log("merhaba dünya!");
+            // console.log(++yas);
+            console.log(isim, soyisim);
+            console.log (tamisim, yas);
+            var yas =20;
+            console.log("Doğum yılı;", buYil + yas) ;
+           // function scrollFn() {
+        //  if (Window.pageY0ffset > 100) {
+          //  document.getElementById("menu").classList.add("menu-scroll");
+        //  } else {
+          //  document.getElementById("menu").classList.remove("menu-scroll");
+          //}
+     //}
+  
